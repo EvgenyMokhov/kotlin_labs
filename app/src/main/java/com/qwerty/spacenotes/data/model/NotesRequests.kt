d0fb7e8faefc6f8dd431d0b637e4c5dca74ee516
EvgenyMokhov <wolfmans.empire@gmail.com>
2025-12-03 01:14:55 +0500

    7 лаба

---
+++ app/src/main/java/com/qwerty/spacenotes/data/model/NotesRequests.kt
@@ -0,0 +1,12 @@
+package com.qwerty.spacenotes.data.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PatchListRequest(val list: List<NoteDto>)
+
+@Serializable
+data class ElementRequest(
+    @SerialName("element") val element: NoteDto
+)
